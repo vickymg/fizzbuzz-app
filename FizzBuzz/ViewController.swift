402df89c073e8480fcac0dd24566925c65e6ac18
Vicky Gray <vickygray26@hotmail.com>
2016-05-23 22:15:58 +0100

    Initial Commit

---
+++ FizzBuzz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FizzBuzz
+//
+//  Created by Victoria Gray on 23/05/2016.
+//  Copyright © 2016 Vicky Gray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
